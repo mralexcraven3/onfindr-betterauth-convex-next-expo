39f548a455bf0d3b7e44b6699064f776a5ec6bb4
Alex Craven <alex.craven00@gmail.com>
2025-09-20 11:48:17 +0100

    initial commit

---
+++ apps/web/src/lib/auth-server.ts
@@ -0,0 +1,6 @@
+import { createAuth } from "@onfindr-betterauth-convex-next-expo/backend/convex/auth";
+import { getToken as getTokenNextjs } from "@convex-dev/better-auth/nextjs";
+
+export const getToken = () => {
+	return getTokenNextjs(createAuth);
+};
